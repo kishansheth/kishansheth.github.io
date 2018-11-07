3ead49ef7154d07f06d3f051382e53522a156b84
kishansheth <kishansheth98@gmail.com>
2018-11-06 22:16:03 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.81101e743af7fba8f21456a5f0d39cee.js"
+  "/precache-manifest.3e7187a91bc0440d22a27f639443e111.js"
 );
 
 workbox.clientsClaim();

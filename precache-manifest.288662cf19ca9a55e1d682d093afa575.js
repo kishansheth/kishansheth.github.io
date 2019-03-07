7e4952fbbb080b12866452890bb5ee08c20e18ef
kishansheth <kishansheth98@gmail.com>
2019-03-07 17:19:17 -0500

    Updates

--- precache-manifest.288662cf19ca9a55e1d682d093afa575.js
+++ precache-manifest.288662cf19ca9a55e1d682d093afa575.js
@@ -24,36 +24,40 @@ self.__precacheManifest = [
     "url": "/static/media/RobotoMono-Light.b82d827b.ttf"
   },
   {
-    "revision": "b93c2ad436ccb747ae415c1e865819a4",
-    "url": "/static/media/RobotoMono-Thin.b93c2ad4.ttf"
-  },
-  {
     "revision": "5a45b286f59824f5d3b2d761871ced29",
     "url": "/static/media/RobotoMono-Medium.5a45b286.ttf"
   },
   {
-    "revision": "c0c4a33786b0278c385d0f647b57490f",
-    "url": "/static/media/RobotoMono-Bold.c0c4a337.ttf"
-  },
-  {
-    "revision": "69ab2827629b34838e37374360bc8d40",
-    "url": "/static/media/white_instagram.69ab2827.svg"
-  },
-  {
-    "revision": "65014891e2e9e71a3fb77e09bb865d90",
-    "url": "/static/media/white_vsco.65014891.svg"
+    "revision": "b93c2ad436ccb747ae415c1e865819a4",
+    "url": "/static/media/RobotoMono-Thin.b93c2ad4.ttf"
   },
   {
     "revision": "c99a3a4b4cb2cfec15f947d518dc0a00",
     "url": "/static/media/FaceOutline-01.c99a3a4b.svg"
   },
   {
-    "revision": "4d574a159014263236e9be12f28e6dfc",
-    "url": "/static/media/white_linkedin.4d574a15.svg"
+    "revision": "c0c4a33786b0278c385d0f647b57490f",
+    "url": "/static/media/RobotoMono-Bold.c0c4a337.ttf"
+  },
+  {
+    "revision": "65014891e2e9e71a3fb77e09bb865d90",
+    "url": "/static/media/white_vsco.65014891.svg"
+  },
+  {
+    "revision": "69ab2827629b34838e37374360bc8d40",
+    "url": "/static/media/white_instagram.69ab2827.svg"
   },
   {
     "revision": "cd6a82fd5f8339a8a953f9b98317ad3e",
     "url": "/static/media/white_github.cd6a82fd.svg"
+  },
+  {
+    "revision": "4d574a159014263236e9be12f28e6dfc",
+    "url": "/static/media/white_linkedin.4d574a15.svg"
+  },
+  {
+    "revision": "db52d8d279d869dafe79e2164bd0e2ca",
+    "url": "/static/media/black_vsco.db52d8d2.svg"
   },
   {
     "revision": "3a8a9f358e1756cc896291b820921107",
@@ -62,10 +66,6 @@ self.__precacheManifest = [
   {
     "revision": "025fd5690db6b6bb870d6868cbf00a13",
     "url": "/static/media/black_linkedin.025fd569.svg"
-  },
-  {
-    "revision": "db52d8d279d869dafe79e2164bd0e2ca",
-    "url": "/static/media/black_vsco.db52d8d2.svg"
   },
   {
     "revision": "74efb98fbb84adee8c3bad29e0b9ea2b",
